2509c9dbd97e38a7197eba191987b7e4053b278f
amal.mohamed <amal.mohamed@bluecrunch.com>
2020-06-23 02:27:09 +0200

    Updates

---
+++ precache-manifest.d5115875f242bf40a47396786ca92141.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f11aefc0c33ab00a0112a0ec53eaeb",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "52b6b76a9e3e71c97efd",
+    "url": "/react-app/static/css/2.4ebad8cb.chunk.css"
+  },
+  {
+    "revision": "79c010872513d91e7513",
+    "url": "/react-app/static/css/main.b817de67.chunk.css"
+  },
+  {
+    "revision": "52b6b76a9e3e71c97efd",
+    "url": "/react-app/static/js/2.6916c971.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.6916c971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c010872513d91e7513",
+    "url": "/react-app/static/js/main.f1272b47.chunk.js"
+  },
+  {
+    "revision": "7d51284e9ec304a68e36",
+    "url": "/react-app/static/js/runtime-main.e5912584.js"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/react-app/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/react-app/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/react-app/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/react-app/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
